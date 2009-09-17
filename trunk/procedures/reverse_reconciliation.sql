89577f2c091fc36c0117aa839334e9f5dc77253d
photomalta.com <photomalta.com@154a753c-a12c-11de-b98f-4500b42dc123>
2009-09-17 17:01:41 +0000

    reverse_reconciliation - enabled error handling

--- trunk/procedures/reverse_reconciliation.sql
+++ trunk/procedures/reverse_reconciliation.sql
@@ -1,3 +1,26 @@
+#######################################################################################
+##                                                                                   ##
+##   This is reverse_reconciliation, used to migrate MySQL user accounts to          ##
+##   Securich.
+##                                                                                   ##
+##   This program was written by Darren Cassar 2009.                                 ##
+##   Feedback and improvements are welcome at:                                       ##
+##   info [at] securich.com / info [at] mysqlpreacher.com                            ##
+##                                                                                   ##
+##   THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED             ##
+##   WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF            ##
+##   MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.                           ##
+##                                                                                   ##
+##   This program is free software; you can redistribute it and/or modify it under   ##
+##   the terms of the GNU General Public License as published by the Free Software   ##
+##   Foundation, version 2.                                                          ##
+##                                                                                   ##
+##   You should have received a copy of the GNU General Public License along with    ##
+##   this program; if not, write to the Free Software Foundation, Inc., 59 Temple    ##
+##   Place, Suite 330, Boston, MA  02111-1307  USA.                                  ##
+##                                                                                   ##
+#######################################################################################
+
 USE securich;
 
 DROP PROCEDURE IF EXISTS reverse_reconciliation;
@@ -38,15 +61,15 @@ CREATE PROCEDURE `securich`.`reverse_reconciliation`()
       DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
 
-/*Exception handler
+
       DECLARE EXIT HANDLER FOR SQLEXCEPTION
       BEGIN
          ROLLBACK;
-         /* The below if statement blocks an alert if reconciliation is not possible due to no users to reconcile (happens normally at the beginning of an installation) 
+         /* The below if statement blocks an alert if reconciliation is not possible due to no users to reconcile (happens normally at the beginning of an installation) */
          IF (SELECT COUNT(*) FROM sec_users) > 0 THEN
             SELECT 'Error occurred - terminating - reverse reconciliation failed';
          END IF;
-      END;*/
+      END;
 
          update sec_config set VALUE = 1 where PROPERTY = 'reverse_reconciliation_in_progress';
 
