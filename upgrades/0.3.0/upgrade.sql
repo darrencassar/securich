dcea3ded208abe4eaec8cafa1106ea8bf93eb625
photomalta.com <photomalta.com@gmail.com>
2010-08-03 22:30:39 +0000

    alter table sec_config

--- upgrades/0.3.0/upgrade.sql
+++ upgrades/0.3.0/upgrade.sql
@@ -1,6 +1,3 @@
-create table sec_configuration
-  ( conf_param varchar(40),
-    conf_value varchar(40)
-  ) engine=myisam;
-
-insert into sec_configuration values ('mode','strict');
+ALTER TABLE `securich`.`sec_config` CHANGE COLUMN `VALUE` `VALUE` VARCHAR(40) NULL  ;
+insert into sec_config (PROPERTY,VALUE) values ('mode','strict');
+insert into sec_config (PROPERTY,VALUE) values ('admin_user',(select substring_index(user(),'@',1)));
