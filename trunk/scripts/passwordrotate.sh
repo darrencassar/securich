d5561dc83e7081e1540eae5a37b64577fa6ae993
photomalta.com@gmail.com <photomalta.com@gmail.com@154a753c-a12c-11de-b98f-4500b42dc123>
2010-09-20 14:25:15 +0000

    passwordrotate update

--- trunk/scripts/passwordrotate.sh
+++ trunk/scripts/passwordrotate.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#######################################################################################
+##                                                                                   ##
+##   This is a script used to email users about password expiry                       ##
+##                                                                                   ##
+##   This program was written by Darren Cassar 2009.                                 ##
+##   Feedback and improvements are welcome at:                                       ##
+##   info [at] securich.com / info [at] mysqlpreacher.com                            ##
+##                                                                                   ##
+##   THIS PROGRAM IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED             ##
+##   WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF            ##
+##   MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.                           ##
+##                                                                                   ##
+##   This program is free software; you can redistribute it and/or modify it under   ##
+##   the terms of the GNU General Public License as published by the Free Software   ##
+##   Foundation, version 2.                                                          ##
+##                                                                                   ##
+##   You should have received a copy of the GNU General Public License along with    ##
+##   this program; if not, write to the Free Software Foundation, Inc., 59 Temple    ##
+##   Place, Suite 330, Boston, MA  02111-1307  USA.                                  ##
+##                                                                                   ##
+#######################################################################################
+
 USER=root
 PASSWORD=
 HOSTNAME=127.0.0.1
@@ -11,6 +33,11 @@ PERIOD=30
 SUBJECT="testing"
 # Email text/message
 EMAILMESSAGE="email.body"
+EMAILLIST="email.body"
+
+#######################################################################################
+# Don't change anything below this line unless you are sure about what you are doing  #
+#######################################################################################
 
 mysql -u$USER -p$PASSWORD -h$HOSTNAME -P$PORT -D securich  -e "
    select us.USERNAME,us.EMAIL_ADDRESS
@@ -20,10 +47,10 @@ mysql -u$USER -p$PASSWORD -h$HOSTNAME -P$PORT -D securich  -e "
       where UPDATE_TIMESTAMP < ADDDATE(NOW(), INTERVAL -$PERIOD DAY) and TYPE='USER'
       ) pr
    where us.ID=usho.US_ID and usho.ID=pr.US_HO_ID
-" --skip-column-names > email.list
+" --skip-column-names > $EMAILLIST
 
 IFS=$'\n'
-for line in $(cat email.list)
+for line in $(cat $EMAILLIST)
 do
    NAME=`echo $line | cut -f 1`
    EMAIL_ADDRESS=`echo $line | cut -f 2`
