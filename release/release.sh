ee37237bfee92ecde8e27a7b0cda976b82b9e0ce
photomalta.com <photomalta.com@gmail.com>
2011-03-03 16:23:46 +0000

    features, changelog etc

--- release/release.sh
+++ release/release.sh
@@ -1,6 +1,6 @@
-rm -rf securich.0.5
-cp -rp securich securich.0.5
-cd securich.0.5
+rm -rf securich.0.5.2
+cp -rp securich securich.0.5.2
+cd securich.0.5.2
 rm -rf testing
 rm -rf securich_install
 rm -rf release
@@ -12,5 +12,5 @@ rm manifest.tmp
 cat MANIFEST.txt | grep -v manifest.tmp > MANIFEST2.txt
 mv MANIFEST2.txt MANIFEST.txt
 cd ..
-tar -cf securich.0.5.tar securich.0.5
-gzip -6 securich.0.5.tar
+tar -cf securich.0.5.2.tar securich.0.5.2
+gzip -6 securich.0.5.2.tar
