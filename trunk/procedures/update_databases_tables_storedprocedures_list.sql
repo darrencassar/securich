6b8109a14913a58e4c4bc74a351681e90335ffce
photomalta.com@gmail.com <photomalta.com@gmail.com@154a753c-a12c-11de-b98f-4500b42dc123>
2011-04-28 21:30:20 +0000

    update_databases_tables_storedprocedures_list optimization

--- trunk/procedures/update_databases_tables_storedprocedures_list.sql
+++ trunk/procedures/update_databases_tables_storedprocedures_list.sql
@@ -44,7 +44,31 @@ CREATE PROCEDURE `securich`.`update_databases_tables_storedprocedures_list`()
      insert into updtdbnames select distinct(db) from mysql.tables_priv;
      insert into updtdbnames select distinct(db) from mysql.procs_priv;
      insert into updtdbnames select distinct(SCHEMA_NAME) from information_schema.SCHEMATA where SCHEMA_NAME != 'information_schema';                                  
-              
+
+     drop temporary table if exists temptbl1;
+     
+     CREATE TEMPORARY TABLE `temptbl1` (
+     `TABLE_SCHEMA` varchar(64) NOT NULL DEFAULT '',
+     `TABLE_NAME` varchar(64) NOT NULL DEFAULT '',
+     KEY `tt_idx` (`TABLE_SCHEMA`,`TABLE_NAME`)
+     ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
+                                
+     insert into temptbl1 select TABLE_SCHEMA, TABLE_NAME
+        from information_schema.tables
+        where table_schema <> 'information_schema';
+          
+     drop temporary table if exists temptbl2;
+                   
+     CREATE TEMPORARY TABLE `temptbl2` (
+     `ROUTINE_SCHEMA` varchar(64) NOT NULL DEFAULT '',
+     `ROUTINE_NAME` varchar(64) NOT NULL DEFAULT '',
+     KEY `tt_idx` (`ROUTINE_SCHEMA`,`ROUTINE_NAME`)
+     ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ;
+     
+     insert into temptbl2 select ROUTINE_SCHEMA, ROUTINE_NAME
+        from information_schema.routines
+        where ROUTINE_SCHEMA <> 'information_schema';
+          
      insert into securich.sec_databases (DATABASENAME)
         select distinct(db)
         from updtdbnames
@@ -54,33 +78,45 @@ CREATE PROCEDURE `securich`.`update_databases_tables_storedprocedures_list`()
            );
 
      insert into securich.sec_tables (TABLENAME)
-        select distinct TABLE_NAME
-        from information_schema.tables
-        where table_schema <> 'information_schema' and
-        TABLE_NAME not in (
-           select TABLENAME
-           from securich.sec_tables
-           );
+     SELECT tbname 
+     FROM (
+        SELECT tbname, location
+        FROM
+         (
+          SELECT TABLE_NAME as tbname, 'new' as location
+          FROM temptbl1 AS S
+          UNION ALL
+          SELECT TABLENAME as tbname, 'old' as location
+          FROM sec_tables AS D
+          WHERE TABLENAME != ''
+        )  AS alias_table
+        GROUP BY tbname
+        HAVING COUNT(*) = 1) abz
+        WHERE location='new';
 
      insert into securich.sec_storedprocedures (STOREDPROCEDURENAME)
-        select distinct SPECIFIC_NAME
-        from information_schema.routines
-        where ROUTINE_SCHEMA <> 'information_schema' and
-        SPECIFIC_NAME not in (
-           select STOREDPROCEDURENAME
-           from securich.sec_storedprocedures
-           );
+     SELECT spname 
+     FROM (
+        SELECT spname, location
+        FROM
+         (
+          SELECT ROUTINE_NAME as spname, 'new' as location
+          FROM temptbl2 AS S
+          UNION ALL
+          SELECT STOREDPROCEDURENAME as spname, 'old' as location
+          FROM sec_storedprocedures AS D
+          WHERE STOREDPROCEDURENAME != ''
+        )  AS alias_table
+        GROUP BY spname
+        HAVING COUNT(*) = 1) abz
+        WHERE location='new';           
 
      insert into sec_db_tb (DB_ID,TB_ID)
         select DB_ID,TB_ID
         from
         (
            select db.ID as DB_ID, tb.ID as TB_ID
-           from sec_databases db, sec_tables tb join (
-              select TABLE_SCHEMA, TABLE_NAME
-              from information_schema.tables
-              where table_schema <> 'information_schema' 
-              ) nms
+           from sec_databases db, sec_tables tb join temptbl1 nms
            where nms.TABLE_SCHEMA = db.DATABASENAME and
            nms.TABLE_NAME = tb.TABLENAME
            UNION ALL
@@ -88,7 +124,7 @@ CREATE PROCEDURE `securich`.`update_databases_tables_storedprocedures_list`()
            from sec_db_tb
         ) tmp
         group by DB_ID, TB_ID
-        having count(*) = 1 ;
+        having count(*) = 1 ; 
 
      insert into sec_db_sp (DB_ID,SP_ID)
         select DB_ID,SP_ID
@@ -96,12 +132,12 @@ CREATE PROCEDURE `securich`.`update_databases_tables_storedprocedures_list`()
         (
            select db.ID as DB_ID, sp.ID as SP_ID
            from sec_databases db, sec_storedprocedures sp join (
-              select ROUTINE_SCHEMA, SPECIFIC_NAME
+              select ROUTINE_SCHEMA, ROUTINE_NAME
               from information_schema.routines
               where ROUTINE_SCHEMA <> 'information_schema' 
               ) nms
            where nms.ROUTINE_SCHEMA = db.DATABASENAME and
-           nms.SPECIFIC_NAME = sp.STOREDPROCEDURENAME
+           nms.ROUTINE_NAME = sp.STOREDPROCEDURENAME
            UNION ALL
            select DB_ID,SP_ID
            from sec_db_sp
